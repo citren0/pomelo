62dc2535a673e537b208cd159002f181267b8d7f
Citren0 <cameron.davis1944@gmail.com>
2024-11-20 14:46:36 -0700

    Changed way AI suggests time.

--- pomelo-website/src/services/humanReadableTimeToFormat.ts
+++ pomelo-website/src/services/humanReadableTimeToFormat.ts
@@ -1,40 +1,10 @@
+import dropdownHours from "@/constants/dropdownHours";
 
 const humanReadableTimeToFormat = (time: string): number =>
 {
-    if (time.includes("AM"))
-    {
-        const hour = parseInt(time.replace("AM", ""));
+    const value = dropdownHours.find((val) => val.display == time);
 
-        if (hour == 12)
-        {
-            return 23;
-        }
-        else
-        {
-            return hour - 1;
-        }
-
-    }
-    else if (time.includes("PM"))
-    {
-        const hour = parseInt(time.replace("PM", ""));
-        const adjustedTime = hour + 11;
-
-        if (hour == 12)
-        {
-            return 11;
-        }
-        else
-        {
-            return adjustedTime;
-        }
-
-    }
-    else
-    {
-        return 0;
-    }
-    
+    return parseInt(value?.value ?? "0");
 };
 
 export default humanReadableTimeToFormat;
