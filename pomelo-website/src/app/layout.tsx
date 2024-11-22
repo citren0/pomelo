1419239d2460227a6501abab75279f062de5f50c
Citren0 <cameron.davis1944@gmail.com>
2024-11-21 19:36:55 -0700

    Changed x script to js file.

--- pomelo-website/src/app/layout.tsx
+++ pomelo-website/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Script from "next/script";
 
 export const metadata: Metadata = {
 	title: "Pomelo",
@@ -14,9 +15,7 @@ const RootLayout = (
 	return (
 		<html lang="en">
 			<head>
-				<script>
-					{"!function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);},s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='https://static.ads-twitter.com/uwt.js',a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');twq('config','orm5f');"}
-				</script>
+				<Script type="text/javascript" async defer src="/x.js" />
 			</head>
 			<link rel="icon" href="/assets/pomelo.png" sizes="any" />
 			<body>
