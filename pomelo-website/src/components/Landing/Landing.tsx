e330888e1f4454b5289d1a015ae25ea9b37c05fd
Citren0 <cameron.davis1944@gmail.com>
2024-11-24 19:06:42 -0700

    Improved landing, hopefully.

--- pomelo-website/src/components/Landing/Landing.tsx
+++ pomelo-website/src/components/Landing/Landing.tsx
@@ -7,23 +7,23 @@ const Landing = () =>
         <>
             <div className="landing-wrapper">
                 <span className="landing-title">
-                    Your own AI-powered productivity coach.&nbsp;
+                    Save time for the things you <span className="landing-title-italic">really</span> care about.&nbsp;
                 </span>
                 <span className="landing-subtitle">
-                    Pomelo allows you to correct bad habits and waste less time online by keeping an eye on your browsing activity.
+                    Your own AI productivity coach will block time-wasting websites to <span className="landing-subtitle-bold">save you time</span>.
                 </span>
 
                 <div className="landing-images-flex">
                     <div className="landing-images-wrapper">
-                        <img src="/assets/chat.png" height={450} width={535} className="landing-img" alt="A product demo showcasing the Pomelo chat functionality." />
+                        <img src="/assets/chat.png" height={673} width={800} className="landing-img" alt="A product demo showcasing the Pomelo chat functionality." />
                         <span className="landing-images-explanation">
-                            Your personal assistant will recommend new productivity strategies and block time-wasting websites whenever it detects you&apos;re slacking.
+                            Your personal assistant will recommend productivity strategies and block time-wasting websites.
                         </span>
                     </div>
                     <div className="landing-images-wrapper">
-                        <img src="/assets/blocked.png" height={450} width={535} className="landing-img" alt="A product demo showcasing the website blocking functionality." />
+                        <img src="/assets/blocked.png" height={673} width={800} className="landing-img" alt="A product demo showcasing the website blocking functionality." />
                         <span className="landing-images-explanation">
-                            When you violate a productivity block, Pomelo will let you know with an un-removable message, discouraging time-wasting behavior.
+                            Productivity rules keep you accountable by blocking websites during certain times of day.
                         </span>
                     </div>
                 </div>
@@ -39,17 +39,17 @@ const Landing = () =>
                     <div className="landing-card">
                         <span className="landing-card-title">Browser Extension</span>
                         <hr className="hr-100" />
-                        <span className="landing-card-text">Keeps an eye on your browsing habits while respecting your privacy.</span>
+                        <span className="landing-card-text">Keeps an eye on your browsing habits and enforces your website blocks.</span>
                     </div>
                     <div className="landing-card">
                         <span className="landing-card-title">AI-powered Insights</span>
                         <hr className="hr-100" />
-                        <span className="landing-card-text">Provides a personal productivity coach to help you be the best you.</span>
+                        <span className="landing-card-text">Provides a personal productivity coach to help you learn from your habits.</span>
                     </div>
                     <div className="landing-card">
                         <span className="landing-card-title">Pomelo Dashboard</span>
                         <hr className="hr-100" />
-                        <span className="landing-card-text">Offers platform where you can make rules to block time-wasting websites.</span>
+                        <span className="landing-card-text">Combines your insights, coach, and habits into one platform to save you time.</span>
                     </div>
                 </div>
 
